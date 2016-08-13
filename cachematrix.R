af74ae4bd5b01f9a6d03665ce4573dabc1d24134
Pavan <pavanpec30@gmail.com>
2016-08-13 15:06:40 -0500

    Programming Assignment2
    Pavan Akula

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#makeCacheMatrix <- function(y = matrix()) {
 
-}
+#}
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+#cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+#}
+##### FUnction 1 ####################################################################################################
+makeCacheMatrix <- function(y = matrix()) {
+  matrix <- NULL
+  set <- function(x) {
+    y <<- x
+    matrix <<- NULL #initialising 
+  }
+  get <- function() y
+  setinverse <- function(inverse) matrix <<- inverse #storing the inverse
+  getinverse <- function() matrix #getting the matirx 
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
+
+##### Function 2 - Solving for inverse ##################################################################################################
+
+cacheSolve <- function(y, ...) {
+  matrix <- y$getinverse()
+  if(!is.null(matrix)) { #checking if the inverse has been calculated before 
+    message("getting cached matrix") 
+    return(matrix)
+  }
+  data <- y$get()
+  matrix <- solve(data, ...) #Solve the mnatrix equation and get the inverse 
+  y$setinverse(matrix)
+  matrix #returning the inverse of the matrix 
+}
+
+
